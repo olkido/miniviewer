a0869f662288493d624979f7fd2bf6f16837ec3b
Olga Diamanti <olga.diam@gmail.com>
2018-07-19 10:59:03 -0700

    pass libigl path as first command argument to compile.sh

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DLIBIGL_DIR=/Users/${USER}/Dropbox/Work/code/libigl/ ../
+cmake -DCMAKE_BUILD_TYPE=Release -DLIBIGL_DIR=$1 ../
 make
 # change this to add to path
 sudo rm /usr/local/bin/miniviewer
