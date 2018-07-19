591f522ebf01da25c0c0d0f9cf9c0863b1ca4d37
Olga Diamanti <olga.diam@gmail.com>
2018-07-19 10:55:42 -0700

    cmake

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release ../
+cmake -DCMAKE_BUILD_TYPE=Release -DLIBIGL_DIR=/Users/${USER}/Dropbox/Work/code/libigl/ ../
 make
 # change this to add to path
 sudo rm /usr/local/bin/miniviewer
